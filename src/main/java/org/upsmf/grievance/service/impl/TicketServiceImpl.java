28c4abcbfa8da705bdc6b81749a82d1a2a402746
rkrahu <rajendra.kishor@tarento.com>
2023-12-27 17:51:11 +0530

    junked by name update

--- src/main/java/org/upsmf/grievance/service/impl/TicketServiceImpl.java
+++ src/main/java/org/upsmf/grievance/service/impl/TicketServiceImpl.java
@@ -612,7 +612,7 @@ public class TicketServiceImpl implements TicketService {
 //      Calculating junkedBy name for ES record
         if (!org.apache.commons.lang.StringUtils.isBlank(ticket.getJunkedBy())) {
             try {
-                Optional<User> userOptional = getOwner(ticket.getAssignedToId());
+                Optional<User> userOptional = getOwner(ticket.getJunkedBy());
 
                 if (userOptional.isPresent()) {
                     junkByName = userOptional.get().getFirstName() + " " + userOptional.get().getLastname();
