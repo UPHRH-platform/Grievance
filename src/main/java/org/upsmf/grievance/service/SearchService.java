f489861d19696355483ef6068e3c48e98ba49455
Shishir Suman <shishir.suman@tarento.com>
2024-01-03 09:21:53 +0530

    bug fixes for mail to secretary for pending tickets

--- src/main/java/org/upsmf/grievance/service/SearchService.java
+++ src/main/java/org/upsmf/grievance/service/SearchService.java
@@ -1,5 +1,6 @@
 package org.upsmf.grievance.service;
 
+import org.upsmf.grievance.dto.SearchDateRange;
 import org.upsmf.grievance.dto.SearchRequest;
 import org.upsmf.grievance.model.es.Ticket;
 import org.upsmf.grievance.model.reponse.TicketResponse;
