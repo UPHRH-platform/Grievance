60fae6c20888369c5844dc2bf425c5884e2202fb
Shishir Suman <shishir.suman@tarento.com>
2024-01-16 20:08:40 +0530

    bug fixes for saving feedback comment in ES

--- src/main/java/org/upsmf/grievance/repository/es/FeedbackRepository.java
+++ src/main/java/org/upsmf/grievance/repository/es/FeedbackRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface FeedbackRepository extends ElasticsearchRepository<Feedback, String> {
 
-    @Query("{'ticket_id': ?0}")
-    List<Feedback> findAllByTicketId(String id);
+    //@Query("{'ticket_id': ?0}")
+    List<Feedback> findAllByTicketId(String ticketId);
 }
