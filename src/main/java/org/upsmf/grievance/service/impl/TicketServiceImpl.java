5d658a5dde4d209bd1323498f4a25fa7715594e3
Shishir Suman <shishir.suman@tarento.com>
2024-01-19 02:59:38 +0530

    adding logic to calculate and set escalation date time based on config value when a ticket is reopened the escalation time is also reset based on config value

--- src/main/java/org/upsmf/grievance/service/impl/TicketServiceImpl.java
+++ src/main/java/org/upsmf/grievance/service/impl/TicketServiceImpl.java
@@ -477,7 +477,8 @@ public class TicketServiceImpl implements TicketService {
         // set incoming values
         setUpdateTicket(updateTicketRequest, ticket);
         // if ticket is getting reopened we will reset escalation date time
-        if(updateTicketRequest.getStatus().name().equalsIgnoreCase("OPEN")
+        if((updateTicketRequest.getStatus().name().equalsIgnoreCase("OPEN")
+                || updateTicketRequest.getStatus().name().equalsIgnoreCase("INVALID"))
                 && oldStatusValue.name().equalsIgnoreCase("CLOSED") ) {
             // sending reopen ticket mail to nodal officer
             log.info("ticket is getting reopened we will reset escalation date time - {}", ticket.getId());
